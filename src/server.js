cd3985cdc13b2a85c2b080bb530ab36054fdbad2
Andre Monello <atmonello@gmail.com>
2019-10-01 22:37:05 -0300

    Commit #3
    - Adicionando Mongoose
    - Adicionando string de conexão com MongoDB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.listen(3000);
-
-app.get('/', (req, res) => {
-  return res.json({ message: "Hello World"});
+mongoose.connect('mongodb+srv://omnistack:omnistack@atmonello-omnistack-9-eid9k.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params
+// req.params = Acessar route params
+// req.body = Acessar corpo da requisição
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
