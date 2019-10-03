f067402f9eba439a61cb3945cd95dd86f2a3bfee
Andre Monello <atmonello@gmail.com>
2019-10-02 21:14:32 -0300

    Commit #8
    - Adicionando virtual para URL da imagem do Spot

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -16,8 +18,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@atmonello-omnistack-9-eid9k.
 // req.params = Acessar route params
 // req.body = Acessar corpo da requisição
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3001);
